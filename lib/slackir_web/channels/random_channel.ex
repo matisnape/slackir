1264c1592a0887920cbafc5328ab4455e0247810
Anna Anks Nowak <matisnape@wp.pl>
2018-12-01 11:49:57 +0100

    Retrieve messages from database

--- lib/slackir_web/channels/random_channel.ex
+++ lib/slackir_web/channels/random_channel.ex
@@ -3,10 +3,21 @@ defmodule SlackirWeb.RandomChannel do
 
   def join("random:lobby", payload, socket) do
     if authorized?(payload) do
+      send self(), :after_join
       {:ok, socket}
     else
       {:error, %{reason: "unauthorized"}}
     end
+  end
+
+  def handle_info(:after_join, socket) do
+    messages =
+      Slackir.Conversations.list_messages()
+      |> Enum.map(fn(m) -> %{message: m.message, name: m.name} end) # Enum iterates over any type of collection, but cannot manipulate the collections
+      # - that's handled on the collection level
+
+    push socket, "messages_history", %{messages: messages}
+    {:noreply, socket}
   end
 
   # Channels can be used in a request/response fashion
@@ -18,7 +29,7 @@ defmodule SlackirWeb.RandomChannel do
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (random:lobby).
   def handle_in("shout", payload, socket) do
-    spawn(Slackir.Conversations, :create_message, [payload])
+    spawn(Slackir.Conversations, :create_message, [payload]) # This creates a separate process, the handling becomes concurrent and independent
     broadcast! socket, "shout", payload
     {:noreply, socket}
   end
