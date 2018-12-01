f98edc68928f253cfb0e2fdf0eafc79765b1e518
Anna Anks Nowak <matisnape@wp.pl>
2018-12-01 11:15:07 +0100

    Persist conversation message

--- lib/slackir_web/channels/random_channel.ex
+++ lib/slackir_web/channels/random_channel.ex
@@ -18,6 +18,7 @@ defmodule SlackirWeb.RandomChannel do
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (random:lobby).
   def handle_in("shout", payload, socket) do
+    Slackir.Conversations.create_message(payload)
     broadcast socket, "shout", payload
     {:noreply, socket}
   end
